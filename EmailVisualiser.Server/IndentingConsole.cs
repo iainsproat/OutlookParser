3bd0d46d81e442c2c0f5366c35eefb8ae58881e2
Iain Sproat <iainsproat@gmail.com>
2014-01-03 11:20:02 +0000

    New project for web server

---
+++ EmailVisualiser.Server/IndentingConsole.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace EmailVisualiser.Server
+{
+    public class IndentingConsole : TextWriter
+    {
+        TextWriter oldConsole;
+        bool doIndent;
+
+        public IndentingConsole()
+        {
+            oldConsole = Console.Out;
+            Console.SetOut(this);
+        }
+
+        public int Indent { get; set; }
+
+        public override void Write(char ch)
+        {
+            if (this.doIndent)
+            {
+                this.doIndent = false;
+                for (int ix = 0; ix < Indent; ++ix)
+                {
+                    oldConsole.Write("  ");
+                }
+            }
+
+            oldConsole.Write(ch);
+            if (ch == '\n')
+            {
+                this.doIndent = true;
+            }
+        }
+
+        public override System.Text.Encoding Encoding
+        {
+            get 
+            { 
+                return oldConsole.Encoding; 
+            }
+        }
+    }
+}
---
+++ EmailVisualiser.Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+using Nancy.Hosting.Self;
+
+namespace EmailVisualiser.Server
+{
+    class Program
+    {
+        public static IndentingConsole Output = new IndentingConsole();
+        private const string startUrl = "http://localhost:62259";
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri(startUrl)))
+            {
+                Console.WriteLine("Launching Email Visualiser.");
+                host.Start();
+                Console.WriteLine("Server has launched.");
+                Console.WriteLine("Launching browser...");
+                Process.Start(startUrl);
+                Console.ReadLine();
+            }
+        }
+    }
+}
