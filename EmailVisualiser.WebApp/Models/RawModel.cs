fea3561476860d0a8bf3a2d780873e6bdfeff572
Iain Sproat <iainsproat@gmail.com>
2014-01-04 20:09:05 +0000

    Raw data can be viewed
    Note that there isn't any formatting on the page yet, it's just a very
    long list-of-lists!

---
+++ EmailVisualiser.WebApp/Models/RawModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using EmailVisualiser.Data;
+
+namespace EmailVisualiser.WebApp.Models
+{
+    public class RawModel
+    {
+        private readonly DataStorage _data;
+
+        public RawModel(DataStorage dataStore)
+        {
+            this._data = dataStore;
+        }
+
+        public IEnumerable<IPersistentEmail> AllEmails
+        {
+            get
+            {
+                return this._data.AllEmails;
+            }
+        }
+    }
+}
---
+++ EmailVisualiser.WebApp/Modules/RawModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EmailVisualiser.WebApp.Models;
+using EmailVisualiser.Data;
+using Nancy;
+
+namespace EmailVisualiser.WebApp.Modules
+{
+    public class RawModule : NancyModule
+    {
+        public RawModule(DataStorage data)
+            :base("/raw")
+        {
+            Get["/"] = parameters =>
+                {
+                    return View["raw", new RawModel(data)];
+                };
+        }
+    }
+}
