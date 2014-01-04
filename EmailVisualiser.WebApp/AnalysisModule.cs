754491042d4e00e6e4b03bd765505fdcce7bab09
Iain Sproat <iainsproat@gmail.com>
2014-01-04 11:25:27 +0000

    Move analysis into separate view

---
+++ EmailVisualiser.WebApp/AnalysisModule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EmailVisualiser.Analysis;
+using Nancy;
+
+namespace EmailVisualiser.WebApp
+{
+    public class AnalysisModule : NancyModule
+    {
+        public AnalysisModule(DataAnalysisEngine analysisEngine)
+            : base("/analysis")
+        {
+            Get["/"] = parameters =>
+            {
+                return View["analysis.cshtml"];
+            };
+
+            Get["/data"] = parameters => //TODO no need to refer to a file anymore!
+            {
+                var dailyEmails = analysisEngine.GetEmailDailyCountSortedByDate();
+                return Response.AsJson(dailyEmails.Select(dailyEmail =>
+                {
+                    return new
+                    {
+                        x = dailyEmail.Item1.ToUniversalTime().Subtract(
+                            new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+                            ).TotalMilliseconds / 1000, //seconds
+                        y = dailyEmail.Item2
+                    };
+                }));
+            };
+        }
+    }
+}
--- EmailVisualiser.WebApp/RootModule.cs
+++ EmailVisualiser.WebApp/RootModule.cs
@@ -4,17 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Nancy;
-
-using EmailVisualiser.Analysis;
 using EmailVisualiser.Data;
 using EmailVisualiser.Models;
+using Nancy;
 
 namespace EmailVisualiser.WebApp
 {
     public class RootModule : NancyModule
     {
-        public RootModule(DataAnalysisEngine analysisEngine)
+        public RootModule()
         {
             Get["/"] = parameters =>
                 {
@@ -28,21 +26,7 @@ namespace EmailVisualiser.WebApp
             Get["/css/{File}"] = parameters =>
                 {
                     return Response.AsCss("Content/" + parameters.File as string);
-                };
-            Get["/data/{File}"] = parameters => //TODO no need to refer to a file anymore!
-                {
-                    var dailyEmails = analysisEngine.GetEmailDailyCountSortedByDate();
-                    return Response.AsJson(dailyEmails.Select(dailyEmail =>
-                        {
-                            return new
-                            {
-                                x = dailyEmail.Item1.ToUniversalTime().Subtract(
-                                    new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
-                                    ).TotalMilliseconds / 1000, //seconds
-                                y = dailyEmail.Item2
-                            };
-                        }));
-                };
+                };            
         }
     }
 }
